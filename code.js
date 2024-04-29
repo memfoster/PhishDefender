ec20743f2cb6df2ade4be81989e761c7ebd41b10
memfoster <166061060+memfoster@users.noreply.github.com>
2024-04-29 08:54:02 -0500

    Update code.js

--- code.js
+++ code.js
@@ -118,7 +118,7 @@ async function main() {
                     clientId: CLIENT_ID,
                     clientSecret: CLIENT_SECRET,
                     refreshToken: REFRESH_TOKEN,
-                    accessToken: token,
+                    accessToken: ACCESS_TOKEN,
                 }
             });
             const mailOptions = {
